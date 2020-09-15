736ba5751c84c473c8227e2373a63fc7ff38911a
Stephan Hoster <stephan@larkshill.org>
2020-09-15 15:39:26 +0200

    initial upload

---
+++ NTFSComplianceCheckMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace NTFSComplianceCheck
+{
+    partial class NTFSComplianceCheckMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.closeButton.Location = new System.Drawing.Point(1107, 719);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(135, 38);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // NTFSComplianceCheckMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 791);
+            this.Controls.Add(this.closeButton);
+            this.Name = "NTFSComplianceCheckMain";
+            this.Text = "NTFS Compliance Check";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
